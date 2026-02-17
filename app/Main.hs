95cdd06a070fdc72cc66caf9d3ffc57739c4ec47
stamnostomp <stamno@pm.me>
2026-02-16 23:34:21 -0700

    updated deps and made socket listener

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main (main) where
 
+import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
+import Control.Concurrent (forkIO)
+import qualified Data.byteString.Char8 as BS 
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  sock <- socket AF_INET Stream defaultProtocol
+  setSocketOption sock ReuseAddr 1
+  bind sock (SockAddrInet 8080 0)
+  listen sock 5
+  putStr "Listening on port 8080"
+  acceptLoop sock
+
+acceptLoop :: Socket -> IO ()
+acceptLoop sock = do
+  (conn, _addr) <- do
+    forkIO (handleConnection conn)
+    acceptLoop sock
--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, base, bytestring, lib, network }:
 mkDerivation {
   pname = "http-server-haskell";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base bytestring network ];
   description = "smalll http server";
   license = lib.licensesSpdx."BSD-3-Clause";
   mainProgram = "http-server-haskell";
